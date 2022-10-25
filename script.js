ad8d9edcdf054a99c69c77f8d74acfa0cfd7d915
Garima <paudelgarima229@gmail.com>
2022-10-25 21:47:33 +0545

    Task is going on

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-const display = document.getElementById('C');
-
-const buttons = document.querySelectorAll('.btn');
-// console.log(buttons);
-buttons.forEach(button =>{
-    button.addEventListener('click',myFunction);
-})
-
-function myFunction(){
-    // console.log("You have clicked");
-    document.getElementById("display").innerHTML = "You have clicked a button";
-}
+let string = "";
+const btn = document.querySelectorAll('.button'); 
